e65e62532e57b6f48f493106faf624a76d0e5da1
Wasim <er.wasim339@gmail.com>
2023-03-20 18:19:26 +0530

    third comment

--- src/components/deliver/Deliver.jsx
+++ src/components/deliver/Deliver.jsx
@@ -1,43 +1,70 @@
-import"./deliver.css"
-import Network from "../../assets/network.png"
-import Security from "../../assets/security.png"
-import Open from "../../assets/open.png"
-import AnyCloud from "../../assets/anycloud.png"
-
+import "./deliver.css";
+import Network from "../../assets/network.png";
+import Security from "../../assets/security.png";
+import Open from "../../assets/open.png";
+import AnyCloud from "../../assets/anycloud.png";
 
 const Deliver = () => {
   return (
     <div className="deliver">
-        <h1>We deliver an enterprise data cloud for any data, anywhere, from the Edge to AI.</h1>
-        <div className="deliver-images">
-          <div className="deliver-image">
+      <h1>
+        We deliver an enterprise data cloud for any data, anywhere, from the
+        Edge to AIss.
+      </h1>
+      {/* <div className="deliver-images-wrapper">
+        <div className="deliver-image">
+          <img src={Network} alt="" />
+          <span>Network</span>
+        </div>
+
+        <div className="deliver-image">
+          <img src={Security} alt="" />
+          <span>Security</span>
+        </div>
+
+        <div className="deliver-image">
+          <img src={Open} alt="" />
+          <span>Open</span>
+        </div>
+
+        <div className="deliver-image">
+          <img src={AnyCloud} alt="" />
+          <span>Any Cloud</span>
+        </div>
+      </div> */}
+
+      <div className="deliver-cards">
+        <div className="deliver-card">
+          <img src={Security} alt="" />
+          <span>Security</span>
+          </div>
+          <div className="deliver-card">
+          <img src={Open} alt="" />
+          <span>Open</span>
+          </div>
+          <div className="deliver-card">
+          <img src={AnyCloud} alt="" />
+          <span>Any Cloud</span>
+          </div>
+          <div className="deliver-card">
           <img src={Network} alt="" />
           <span>Network</span>
           </div>
+        {/* <div className="deliver-card">2</div>
+        <div className="deliver-card">3</div>
+        <div className="deliver-card">4</div>
+        <div className="deliver-card">5</div>
+        <div className="deliver-card">6</div>
+        <div className="deliver-card">7</div>
+        <div className="deliver-card">8</div>
+        <div className="deliver-card">9</div>
+        <div className="deliver-card">10</div>
+        <div className="deliver-card">11</div>
+        <div className="deliver-card">12</div> */}
 
-          <div className="deliver-image">
-          <img src={Security} alt="" />
-          <span>Network</span>
-          </div>
-
-          <div className="deliver-image">
-          <img src={Open} alt="" />
-          <span>Network</span>
-          </div>
-
-          <div className="deliver-image">
-          <img src={AnyCloud} alt="" />
-          <span>Network</span>
-          </div>
-          
-{/*            
-            <img src={Security} alt="" />
-            <img src={Open} alt="" />
-            <img src={AnyCloud} alt="" /> */}
-        </div>
-      
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Deliver
+export default Deliver;
--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,11 +1,14 @@
-import"./hero.css";
+import "./hero.css";
+import Img from '../../assets/office.jpg'
 
 const Hero = () => {
   return (
-    <div className="hero">
-      
+    <div className="hero"  style={{backgroundImage: `url(${Img})`, width: "100%", backgroundSize:"cover"}}>
+      <div className="overlay"></div>
+      <h1> Welcome To The World of Opportunities !</h1>
+      <h2>Enable the AI-first Enterprise with CloudAge</h2>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
